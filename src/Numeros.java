e85de204d0ba6090adbdde55e2caaa0fc0236922
zanutt <matszg@live.com>
2024-05-10 19:21:53 -0300

    Ajustando o tipo das variaveis para melhor execuçao do codigo

--- src/Numeros.java
+++ src/Numeros.java
@@ -1,10 +1,11 @@
 public class Numeros {
 
     public static void main(String[] args){
-        int nota1 = 90;
-        int nota2 = 70;
-        int nota3 = 50;
-        int nota4 = 88;
-        System.out.println("A média das notas é igual a "+ ((nota1 + nota2 + nota3+ nota4)/4));
+        double nota1 = 90;
+        double nota2 = 70;
+        double nota3 = 50;
+        double nota4 = 88;
+        double resultado = ((nota1 + nota2 + nota3 + nota4)/4);
+        System.out.println("A média das notas é igual a "+ resultado);
     }
 }
