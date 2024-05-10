a172756d072076bc3aed0f7aad9117e4717e5920
zanutt <matszg@live.com>
2024-05-10 18:36:46 -0300

    Variaveis numericas Basicas Sistema de médias

---
+++ src/Numeros.java
@@ -0,0 +1,10 @@
+public class Numeros {
+
+    public static void main(String[] args){
+        int nota1 = 90;
+        int nota2 = 70;
+        int nota3 = 50;
+        int nota4 = 88;
+        System.out.println("A média das notas é igual a "+ ((nota1 + nota2 + nota3+ nota4)/4));
+    }
+}
