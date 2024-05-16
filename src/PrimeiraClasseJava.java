601ef15966c5c5224bcf806dca32edad2e5bdcc7
zanutt <matszg@live.com>
2024-05-16 19:57:24 -0300

    OOP com criaçao de classe aluno com atributos e getters/setters e metodos fazendo o calculador de média

--- src/PrimeiraClasseJava.java
+++ src/PrimeiraClasseJava.java
@@ -1,10 +1,24 @@
 package ConhecendoLinguagem.src;
 
+import ConhecendoLinguagem.src.classes.Aluno;
+
 public class PrimeiraClasseJava {
 
     /* Main é o metodo auto executavel do java */
     public static void main(String[] args) {
-        /* Escreve o texto no console */
-        System.out.println("Meu Primeiro Programa no Javinha");
+
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome("João");
+        aluno1.setNota1(90);
+        aluno1.setNota2(80.8);
+        aluno1.setNota3(70.9);
+        aluno1.setNota4(90.7);
+
+        System.out.println("Nome é = "+ aluno1.getNome());
+        System.out.println("A média do aluno é = "+ aluno1.getMediaNota());
+        System.out.println("Resultado = "+(aluno1.getAlunoAprovado()? "Aprovado" : "Reprovado"));
+
+
+        Aluno aluno2 = new Aluno();
     }
 }
---
+++ src/classes/Aluno.java
@@ -0,0 +1,125 @@
+package ConhecendoLinguagem.src.classes;
+
+public class Aluno {
+    String nome;
+    int idade;
+    String dataNascimento;
+    String registroGeral;
+    String numeroCpf;
+    String nomeMae;
+    String nomePai;
+    String dataMatricula;
+    double nota1;
+    double nota2;
+    double nota3;
+    double nota4;
+
+
+/*Método set recebe dados*/
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getNome(){
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getRegistroGeral() {
+        return registroGeral;
+    }
+
+    public void setRegistroGeral(String registroGeral) {
+        this.registroGeral = registroGeral;
+    }
+
+    public String getNumeroCpf() {
+        return numeroCpf;
+    }
+
+    public void setNumeroCpf(String numeroCpf) {
+        this.numeroCpf = numeroCpf;
+    }
+
+    public String getNomeMae() {
+        return nomeMae;
+    }
+
+    public void setNomeMae(String nomeMae) {
+        this.nomeMae = nomeMae;
+    }
+
+    public String getNomePai() {
+        return nomePai;
+    }
+
+    public void setNomePai(String nomePai) {
+        this.nomePai = nomePai;
+    }
+
+    public String getDataMatricula() {
+        return dataMatricula;
+    }
+
+    public void setDataMatricula(String dataMatricula) {
+        this.dataMatricula = dataMatricula;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public double getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(double nota3) {
+        this.nota3 = nota3;
+    }
+
+    public double getNota4() {
+        return nota4;
+    }
+
+    public void setNota4(double nota4) {
+        this.nota4 = nota4;
+    }
+    /*Método que rotorna media do aluno*/
+    public double getMediaNota(){
+        return (nota1 + nota2 + nota3 + nota4) / 4;
+    }
+    public boolean getAlunoAprovado(){
+        double media = this.getMediaNota();
+        if (media >= 70){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
