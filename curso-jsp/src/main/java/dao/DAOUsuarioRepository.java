6e53fd04f515b7e3a59868bbf1c679491426a405
zanutt <matszg@live.com>
2024-09-09 04:35:18 -0300

    Começado metodo de consulta no banco do SQL

--- curso-jsp/src/main/java/dao/DAOUsuarioRepository.java
+++ curso-jsp/src/main/java/dao/DAOUsuarioRepository.java
@@ -2,6 +2,7 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import connection.SingleConnectionBanco;
 import model.ModelLogin;
@@ -14,7 +15,7 @@ public class DAOUsuarioRepository {
 		connection = SingleConnectionBanco.getConnection();		
 	}
 	
-	public void gravarUsuario(ModelLogin objeto) throws Exception{
+	public ModelLogin gravarUsuario(ModelLogin objeto) throws Exception{
 		
 		String sql = "INSERT INTO model_login(login, senha, nome, email) VALUES (?, ?, ?, ?);";
 		PreparedStatement statement = connection.prepareStatement(sql);
@@ -27,7 +28,29 @@ public class DAOUsuarioRepository {
 		statement.execute();
 		connection.commit();
 		
+		return this.consultaUsuario(objeto.getLogin());
+		
 	}
 	
+	public ModelLogin consultaUsuario(String login) throws Exception {
+		
+		ModelLogin modelLogin = new ModelLogin();
+		
+		String sql = "select * from model_login where upper(login) = upper('"+ login +"')";
+		PreparedStatement statement = connection.prepareStatement(sql);
+		
+		ResultSet resultado = statement.executeQuery();
+		
+		while (resultado.next()) /*se tem result*/ {
+			modelLogin.setId(resultado.getLong("id"));
+			modelLogin.setLogin(resultado.getString("login"));
+			modelLogin.setEmail(resultado.getString("email"));
+			modelLogin.setSenha(resultado.getString("senha"));
+			modelLogin.setNome(resultado.getString("nome"));
+		}
+		
+		return modelLogin;
+		
+	}
 
 }
